181404b843d99827b10c335fc842395dca20b4d9
StevenIQ <ciobanita16@gmail.com>
2019-03-26 23:33:46 +0100

    Unity test + some updates

--- starting_kit/sample_code_submission/UnitTest.py
+++ starting_kit/sample_code_submission/UnitTest.py
@@ -1,5 +1,5 @@
-import unittest
-from data_io import write
+#import unittest
+#from data_io import write
 from model import model
 import pickle
 import numpy as np   # We recommend to use numpy arrays
@@ -7,11 +7,11 @@ from os.path import isfile
 from sklearn.base import BaseEstimator
 import tobit
 import pandas as pd
-import zePreproDeLaMort
+import zPreprocessing as zprepro
 from data_io import read_as_df
 
 
-class Test(unittest):
+class Test():
     
     """
     On teste si les il y a des donnees cesurees apres le preprocessing
@@ -19,12 +19,14 @@ class Test(unittest):
     def testPrepro():
         filename1 = "public_data/Mortality_train.data"
         dataset = np.loadtxt(filename1, delimiter=" ")
-        prepro = zePreproDeLaMort()
+        prepro = zprepro.zePreproDeLaMort()
         fitTab = prepro.fit(dataset)
         transformTab = prepro.transform(fitTab)
-        for i in transformTab:
-            self.assertEqual(i, 1, msg="Censored")
-        self.assertRaises(ValueError, prepro.transform(fitTab))
+        for i in range(0,len(transformTab)):
+            if i == 1 :
+                print(" is Censored",i)
+                return False
+        return True
     """
     On verifie que chaque 
     """

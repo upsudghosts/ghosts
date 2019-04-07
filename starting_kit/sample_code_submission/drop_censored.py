beaab01624b23d0a7316f8c59d2da7a8cc3f65b4
StevenIQ <ciobanita16@gmail.com>
2019-04-07 23:59:27 +0200

    changed censored : Y==1=

--- starting_kit/sample_code_submission/drop_censored.py
+++ starting_kit/sample_code_submission/drop_censored.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def drop_censored(X, Y):
-    censored_indexes = np.where(Y==0)[0] #age != 0
+    censored_indexes = np.where(Y==1)[0] #age != 0
     X_uncensored = np.delete(X, censored_indexes, axis=0)
     Y_uncensored = np.delete(Y, censored_indexes, axis=0)
     return X_uncensored, Y_uncensored 
